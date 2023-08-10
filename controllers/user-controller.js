c6fd31cdd2b296aa56d5504c7ccfaa1ea3a18492
Jyothi1887 <fordfiremj@gmail.com>
2023-08-10 15:50:39 +0530

    removed uuid commit

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,4 +1,4 @@
-const { v4: uuidv4 } = require('uuid');
+
 const { validationResult } = require('express-validator');
 const User = require('../models/user');
 const HttpError = require('../models/http-error');
